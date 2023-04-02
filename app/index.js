cfae254bc159fcc3625dbf4d6217241563684e4f
unknown <seunkikiowo@gmail.com>
2023-04-02 16:47:06 +0100

    search input done

--- app/index.js
+++ app/index.js
@@ -16,7 +16,7 @@ const Home = () => {
                     <ScreenHeaderBtn iconUrl={ icons.menu} dimension="60%" />
                 ),  
                 headerRight: () => (
-                    <ScreenHeaderBtn iconUrl={ icons.profile} dimension="1000%" />
+                    <ScreenHeaderBtn iconUrl={ images.profile} dimension="100%" />
                 ), 
                 headerTitle: "",
                 headerTitleAlign: 'center'
