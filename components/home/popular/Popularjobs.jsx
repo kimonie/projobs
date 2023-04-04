8b777a2119348978427aeab23f7db3743cd70e22
unknown <seunkikiowo@gmail.com>
2023-04-05 00:34:21 +0100

    Wahala o, no error message for the wahala

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
 import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
 import { useRouter } from 'expo-router';
 
@@ -8,6 +8,9 @@ import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 
 const Popularjobs = () => {
   const router = useRouter();
+  const isLoading = false;
+  const error = false;
+
 
   return (
     <View style={styles.container}>
@@ -17,6 +20,29 @@ const Popularjobs = () => {
           <Text style={styles.headerBtn}>Show all</Text>
         </TouchableOpacity>
       </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList 
+            data={[1,2,3,4,]}
+            renderItem={({ item }) => {
+              <PopularJobCard 
+                item={item}
+              />
+            }}
+            keyExtractor={ item => item}
+            contentContainerStyle={{paddingBottom: SIZES.medium}}
+            horizontal
+          />
+        )}
+
+      </View>
+
+
     </View>
   )
 }
